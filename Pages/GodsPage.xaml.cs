1dd13fb2c9caaee6744369e2239de6c6d1b34893
Anya-jpg <71765648+Anya-jpg@users.noreply.github.com>
2021-11-11 20:13:35 +0300

    Create SidebarMenu and Pages
    11.11.21 Меню было взято из примера, поэтому нет правильных картинок и нужного дизайна. Добавлен фрейм, в котором меняются страницы при выборе вкладок.

---
+++ Pages/GodsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EgyptianDictionary.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для GodsPage.xaml
+    /// </summary>
+    public partial class GodsPage : Page
+    {
+        public GodsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
