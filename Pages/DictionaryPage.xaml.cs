1fe5e2768dda544f6d07bbd1e949cd5efbcfa047
Anya-jpg <71765648+Anya-jpg@users.noreply.github.com>
2021-11-17 22:49:52 +0300

    Working on design

--- Pages/DictionaryPage.xaml.cs
+++ Pages/DictionaryPage.xaml.cs
@@ -16,13 +16,45 @@ using System.Windows.Shapes;
 namespace EgyptianDictionary.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для DictionaryPage.xaml
+    /// Interação lógica para MainWindow.xam
     /// </summary>
     public partial class DictionaryPage : Page
     {
         public DictionaryPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            GridCursor.Margin = new Thickness(10 + (30 * index), 0, 0, 0);
+
+            switch (index)
+            {
+                case 0:
+                    GridMain.Background = Brushes.Aquamarine;
+                    break;
+                case 1:
+                    GridMain.Background = Brushes.Beige;
+                    break;
+                case 2:
+                    GridMain.Background = Brushes.CadetBlue;
+                    break;
+                case 3:
+                    GridMain.Background = Brushes.DarkBlue;
+                    break;
+                case 4:
+                    GridMain.Background = Brushes.Firebrick;
+                    break;
+                case 5:
+                    GridMain.Background = Brushes.Gainsboro;
+                    break;
+                case 6:
+                    GridMain.Background = Brushes.HotPink;
+                    break;
+            }
         }
     }
 }
